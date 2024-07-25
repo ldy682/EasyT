34d277c7dd3bfea1f87b0a7f5887e164963e2a3b
Lew Daniel Yang <lewdanielyang@gmail.com>
2024-07-25 16:05:52 -0700

    Made the cursor wider

--- EasyTQt/mainwindow.cpp
+++ EasyTQt/mainwindow.cpp
@@ -1,15 +1,26 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "terminaltextedit.h"
+#include <QWidget>
+#include <QDockWidget>
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    this->setStyleSheet("background-color: beige;");
+
     ui->setupUi(this);
     TerminalTextEdit *textEdit = new TerminalTextEdit(this);
-    // setCentralWidget(textEdit);
-}
+    setCentralWidget(textEdit);
+
+    QDockWidget *dock = new QDockWidget("DockWidget", this);
+    addDockWidget(Qt::LeftDockWidgetArea, dock);
+
+    QWidget *dockContent = new QWidget(dock);
+    QVBoxLayout *dockLayout = new QVBoxLayout(dockContent);
+    // dock->add}
 
 MainWindow::~MainWindow()
 {
--- EasyTQt/terminaltextedit.cpp
+++ EasyTQt/terminaltextedit.cpp
@@ -1,9 +1,11 @@
 #include "terminaltextedit.h"
 #include <QFont>
+#include <QTextCursor>
 
 TerminalTextEdit::TerminalTextEdit(QWidget *parent) : QTextEdit(parent) {
-    QFont font("Monospace", 12);
-    this->setFont(font);
+    // QFont font("Monospace", 12);
+    this->setFont(QFont("Monospace", 12));
+    this->setCursorWidth(12);
 }
 
 TerminalTextEdit::~TerminalTextEdit(){
