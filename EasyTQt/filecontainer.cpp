c92ff4beb79a2ea22755efddf199a81b8989dd3c
Lew Daniel Yang <lewdanielyang@gmail.com>
2024-07-25 21:31:55 -0700

    Fixed the spacing of the files and removed the borders of the QLineEdit

--- EasyTQt/filecontainer.cpp
+++ EasyTQt/filecontainer.cpp
@@ -3,6 +3,7 @@
 FileContainer::FileContainer(QWidget *parent, QString filename): QLineEdit(parent) {
     this->setReadOnly(true);
     this->setText(filename);
+    this->setFrame(false);
 }
 
 FileContainer::~FileContainer(){
--- EasyTQt/mainwindow.cpp
+++ EasyTQt/mainwindow.cpp
@@ -7,6 +7,8 @@
 #include <QVBoxLayout>
 #include <QRect>
 #include <QLineEdit>
+#include <QSpacerItem>
+#include <QMargins>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -21,14 +23,22 @@ MainWindow::MainWindow(QWidget *parent)
     // set up for file bar on the left
     QDockWidget *dockWidget = new QDockWidget("Files Location", this);
     dockWidget->setFeatures(QDockWidget::DockWidgetMovable);
+    addDockWidget(Qt::LeftDockWidgetArea, dockWidget);
 
     QWidget *widgetContainer = new QWidget(dockWidget);
     QVBoxLayout *vBox = new QVBoxLayout(widgetContainer);
+    vBox->setContentsMargins(0,0,0,0);
+
     FileContainer *file = new FileContainer(widgetContainer, "smth");
+    FileContainer *file2 = new FileContainer(widgetContainer, "smth2");
+
     vBox->addWidget(file);
+    vBox->addWidget(file2);
+    vBox->addStretch();
+
 
     dockWidget->setWidget(widgetContainer);
-    addDockWidget(Qt::LeftDockWidgetArea, dockWidget);
+
 }
 
 MainWindow::~MainWindow()
